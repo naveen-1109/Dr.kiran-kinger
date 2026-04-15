cac2ea96d0b75348d7001d7827edbc756579442b
naveen-1109 <navinaveen1109@gmail.com>
2026-04-15 17:35:35 +0530

    Initial project upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,33 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import TrustSection from "@/components/TrustSection";
+import ServicesSection from "@/components/ServicesSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import HealthInsightsSection from "@/components/HealthInsightsSection";
+import ChildGutHealthSection from "@/components/ChildGutHealthSection";
+
+import TestimonialsSection from "@/components/TestimonialsSection";
+import GallerySection from "@/components/GallerySection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <TrustSection />
+      <ServicesSection />
+      <WhyChooseUs />
+      <HealthInsightsSection />
+      <ChildGutHealthSection />
+
+      <TestimonialsSection />
+      <GallerySection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
